ababcfc27f29b479b0f8cf368de0b7774f6eb4a4
Erick NGOOH <engooh@e2r3p1.clusters.42paris.fr>
2022-02-01 18:57:05 +0100

    ft_mediane_mediane

--- ft_mediane.c
+++ ft_mediane.c
@@ -6,102 +6,79 @@
 /*   By: engooh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:46:16 by engooh            #+#    #+#             */
-/*   Updated: 2022/01/31 16:25:56 by engooh           ###   ########.fr       */
+/*   Updated: 2022/02/01 18:53:23 by engooh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
-int	**ft_create_tabs(int len, t_stack *stack, int size)
+int ft_mediane(int *tabs, int start, int mil, int end)
 {
-	int	i;
-	int	j;
-	int	**tabs;
-
-	tabs = malloc(sizeof(int *) * size);
-	if (!tabs)
-		return (NULL);
-	j = -1;
-	tabs[--size] = 0;
-	while (++j < size)
-	{
-		tabs[j] = malloc(sizeof(int) * 5);
-		if (!tabs[j])
-			return (NULL);
-		i = -1;
-		while (++i < 5 && len--)
-		{
-			tabs[j][i] = stack->content;
-			stack = stack->next;
-		}
-	}
-	return (tabs);
+	if (tabs[start] < tabs[mil] && tabs[start] > tabs[end])
+		return (start);
+	else if (tabs[start] > tabs[mil] && tabs[start] < tabs[end])
+		return (start);
+	else if (tabs[end] < tabs[mil] && tabs[end] > tabs[start])
+		return (end);
+	else if (tabs[end] > tabs[mil] && tabs[end] < tabs[start])
+		return (end);
+	else if (tabs[mil] < tabs[end] && tabs[mil] > tabs[start])
+		return (mil);
+	else if (tabs[mil] > tabs[end] && tabs[mil] < tabs[start])
+		return (mil);
+	return (end);
 }
 
-int	ft_get_mediane(int *tabs, int size, int i)
+int *ft_quicksort(int *tabs, int size)
 {
-	int min;
-	int max;
-	int tab_index[3];
+	int med;
+//	int max;
+	int swp;
+	int i;
+	int j;
 
-	if (size < 5)
-		return (size);
-	min = 0;
-	max = 0;
-	tab_index[0] = 0;
-	tab_index[1] = size / 2;
-	tab_index[2] = size - 1;
-	while (++i < 3)
+	med = ft_mediane(tabs, 0, (size -1) / 2, size - 1);
+	//printf("%d", tabs[med]);
+	i = -1;
+	j = --size;
+	while (++i < j)
 	{
-			if (tabs[min] > tabs[tab_index[i]])
-				min = tab_index[i];
-			if (tabs[max] < tabs[tab_index[i]])
-				max = tab_index[i];
+		while (tabs[i] < tabs[med] && i < med)
+			i++;
+		while (tabs[j] > tabs[med] && j > med)
+			--j;
+		if (tabs[i] > tabs[j] && (i >= med))
+		{
+			if (tabs[i] == tabs[med])
+				med = i;
+			else if (tabs[j] == tabs[med])
+				med = j;
+			swp = tabs[j];
+			tabs[j] = tabs[i];
+			tabs[i] = swp;
+			j--;
+			i++;
+		}
 	}
 	i = -1; 
-	while (++i < 3)
-		if (tabs[tab_index[i]] != tabs[min] && tabs[tab_index[i]] != tabs[max])
-			return(tab_index[i]);
-	return (-1);
-}
-
-int ft_quicksort(int *tabs, int size)
-{
-	int	i;
-	int swp;
-	int	piv;
-
-	i = 0;
-	piv = ft_get_mediane(tabs, size, 0);
 	while (++i < size)
-		if (tabs[piv] < tabs[i] && i < piv)
-		{
-			swp = tabs[i];
-			tabs[i] = tabs[piv];
-			tabs[piv] = swp;
-		}
-	if (end > 1)
-		return(ft_quicksort(tabs + 0, piv - 1) + ft_quicksort(tabs + piv, size - piv));
-	return (1);
+		printf(" %d ", tabs[i]);
+	return (tabs);
 }
 
 int ft_mediane_mediane(int size, t_stack *stack)
 {
-	int	i;
-	int	j;
-	int	**tabs;
+	int *tabs;
+	int i;
 
-	tabs = ft_create_tabs(size, stack, ((size) / 5) + 2);
+	i = size;
+	tabs = malloc(sizeof(int) * (size));
 	if (!tabs)
 		return (0);
-	ft_quicksort(tabs[0], size);
-	i = -1;	
-	while (tabs[++i] != 0)
-	{	
-		j = 0; 
-		while (++j < 5 && size--)
-			j++;
-		free(tabs[i]);
+	while (--size >= 0)
+	{
+		stack = stack->prev;
+		tabs[size] = stack->content;
 	}
-	free(tabs);
+	tabs = ft_quicksort(tabs, i);
 	return (0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: engooh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 14:40:33 by engooh            #+#    #+#             */
-/*   Updated: 2022/01/30 22:39:34 by lazarus          ###   ########.fr       */
+/*   Updated: 2022/02/01 16:05:29 by engooh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -19,7 +19,7 @@ int	main(int ac, char **av)
 	stack_a = ft_init_stack(ac, av);
 //	stack_b = NULL;
 //	ft_push(&stack_a, &stack_b, "pa\n");
-//	printf("%d*\n", ft_stacklen(stack_a));
+// printf("%d*\n", ft_stacklen(stack_a));
 	ft_mediane_mediane(ft_stacklen(stack_a), stack_a);
 	ft_delstack(stack_a, stack_a->prev);
 	return (0);
